18cac426cc8ebfe389eb5fc1401edc8df2554838
Felix Geilert <root92@live.de>
2022-04-18 09:40:52 +0200

    Feat: Started implementation of unit tests for the wrapper

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -1,3 +1,37 @@
 '''Test Code for the wrapper class'''
 
-# TODO: implement
+from datetime import tzinfo, datetime
+import time
+
+try:
+    import zoneinfo
+except ImportError:
+    import backports.zoneinfo as zoneinfo
+
+from time_helper import DateTimeWrapper, localize_datetime
+
+def test_create():
+    '''Tests the creation of various wrapper classes'''
+    # check the basic creation from datetime and equality checks
+    dt = datetime.now()
+    wt = DateTimeWrapper(dt)
+
+    assert wt == dt
+    assert wt() == dt
+    # negative check
+    time.sleep(0.5)
+    assert wt != datetime.now()
+
+    # test creation of datetimes
+    wt = DateTimeWrapper(dt, "Asia/Kolkata")
+    assert wt == localize_datetime(dt, "Asia/Kolkata")
+
+
+def test_call():
+    '''Tests if inner datetime objects are mapped to the corret functions.'''
+    # get a datetime
+    dt = datetime(2022, 10, 19, 12, 00)
+    wt = DateTimeWrapper(dt)
+
+    # compare data
+    wt.isoformat()
--- time_helper/wrapper.py
+++ time_helper/wrapper.py
@@ -5,20 +5,41 @@ This makes many of the functions first class citizens and can easily expose the 
 '''
 
 from datetime import datetime
-from typing import Any
+from typing import Any, Union
+try:
+    from zoneinfo import ZoneInfo
+except ImportError:
+    from backports.zoneinfo import ZoneInfo
 
-from time_helper.convert import any_to_datetime
+from time_helper.convert import any_to_datetime, localize_datetime
 from time_helper.ops import time_diff
 
 
 class DateTimeWrapper():
-    def __init__(self, dt) -> None:
+    def __init__(self, dt: Union[datetime, str, Any], timezone: Union[str, ZoneInfo] = None) -> None:
+        # retrieve the datetime
         self.dt = any_to_datetime(dt)
+
+        # check if a timezone is set
+        if timezone:
+            self.dt = localize_datetime(self.dt, timezone)
 
     def __call__(self, *args: Any, **kwds: Any) -> datetime:
         if not args and not kwds:
             return self.dt
-        # TODO: call action on the internal datetime object
+
+        # TODO: add code that allows to localize?
+
+    def __getattribute__(self, __name: str) -> Any:
+        if hasattr(self.dt, __name):
+            return getattr(self.dt, __name)
+        return None
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, DateTimeWrapper):
+            return self.dt == __o.dt
+        return self.dt == __o
+
 
     # TODO: overload operators
     def __sub__(self, other):
